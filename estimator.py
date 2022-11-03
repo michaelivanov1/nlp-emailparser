94b025e7aed6463ef1ad175c7ee01e13b498850b
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2022-11-03 14:27:24 -0400

    refactored file reading

--- estimator.py
+++ estimator.py
@@ -1,40 +1,28 @@
 import spacy
-# regex import
-import re
-
 nlp = spacy.load('en_core_web_sm')
-# doc = nlp(u'Hi Dave, I’d like to invest $10,000 with Microsoft and another $15,000 with Amazon. Thanks, Tom.')
 
-# read & write text file
-read_file_name = "EmailLog.txt"
-write_file_name = "ParsedEmails.txt"
-read_file_text = open(read_file_name).read()
-doc = nlp(read_file_text)
+# holds raw data from EmailLog.txt
+all_data = ""
+# read in all data from EmailLog.txt to a string
+f = open('EmailLog.txt', 'r', encoding='utf-8')
+for i in f.readlines():
+	all_data += i
+f.close()
 
-all_emails = []
+doc = nlp(all_data)
 
-with open(write_file_name, "wt") as txt_file: 
-    for token in doc:
-        if(token.like_email): 
-            all_emails.append(token.text + "," + token.pos_ + "," + token.tag_ + "," + token.dep_ + "," + str(spacy.explain(token.dep_)))
-            txt_file.write(token.text + "," + token.pos_ + "," + token.tag_ + "," + token.dep_ + "," + str(spacy.explain(token.dep_)) + '\n') 
-print(write_file_name + " file written")
-
-print(all_emails)
-
-# read spaCy data from a text file
-f = open(write_file_name, mode='r', encoding='utf-8')
-for line in f:
-	line = line.strip('\n')
-	print(line)
-
+# save spaCy data to a text file
+f = open('ParsedData.txt', mode='wt', encoding='utf-8')
+for token in doc:
+	f.write(token.text + "," + token.pos_ + "," + token.tag_ + "," + token.dep_ + "," + str(spacy.explain(token.dep_)) + '\n')
 f.close()
 
 
 # read spaCy data from a text file
-# f = open('demo.txt', mode='r', encoding='utf-8')
-# for line in f:
-# 	line = line.strip('\n')
-# 	# print(line)
+f = open('ParsedData.txt', mode='r', encoding='utf-8')
+for line in f:
+	line = line.strip('\n')
+    # start parsing here
+    
 
-# f.close()
+f.close()
